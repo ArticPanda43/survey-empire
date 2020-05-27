dfc37c5a0de521bfe168ad9fa2bc7e2e19986919
Stefano Bussandri <articpanda43@gmail.com>
2020-05-27 01:12:40 +0100

    changed sidebar

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module SurveyEmpire
       ENV[key] = value
     end unless config_file.nil?
 
+    config.stripe.publishable_key = ''
+
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
